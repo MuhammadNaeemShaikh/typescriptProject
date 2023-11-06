f0b1bbc20c30775d5fa7fd80bc038cea6eed2afb
NaeemShaikho <ns265331@gmail.com>
2023-11-06 10:56:16 -0800

    quiz done

--- typescript/quiz/index.ts
+++ typescript/quiz/index.ts
@@ -1,13 +1,4 @@
 import inquirer from 'inquirer';
-import chalk from 'chalk';
-
-// let fName = await inquirer.prompt({
-//   type: 'input',
-//   name: 'fullName',
-//   message: 'What is Your Name ?',
-// });
-
-// console.log(chalk.bold.blue(fName.fullName));
 
 const apiUrl: string =
   'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple';
@@ -18,8 +9,37 @@ let fetchData = async (data: string) => {
   return res;
 };
 
-let res = await fetchData(apiUrl);
+let data = await fetchData(apiUrl);
+
 
 let startQuiz = async () => {
   let score: number = 0;
+
+  let fName = await inquirer.prompt({
+    type: 'input',
+    name: 'fullName',
+    message: 'What is Your Name ?',
+  });
+
+  for (let i = 0; i < data.results.length; i++) {
+    let answer = [
+      ...data.results[i].incorrect_answers,
+      data.results[i].correct_answer,
+    ];
+
+    let ans = await inquirer.prompt({
+      type: 'list',
+      name: 'quiz',
+      message: data.results[i].question,
+      choices: answer.map((val: any) => val),
+    });
+
+    if (ans.quiz === data.results[i].correct_answer) {
+      ++score;
+    }
+  }
+
+  console.log(`${fName.fullName} your score is ${score}`);
 };
+
+startQuiz();
--- typescript/quiz/src/index.js
+++ typescript/quiz/src/index.js
@@ -1,17 +1,33 @@
-// let fName = await inquirer.prompt({
-//   type: 'input',
-//   name: 'fullName',
-//   message: 'What is Your Name ?',
-// });
-// console.log(chalk.bold.blue(fName.fullName));
+import inquirer from 'inquirer';
 const apiUrl = 'https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple';
 let fetchData = async (data) => {
     let fetchQuiz = await fetch(data);
     let res = await fetchQuiz.json();
     return res;
 };
-let res = await fetchData(apiUrl);
+let data = await fetchData(apiUrl);
 let startQuiz = async () => {
     let score = 0;
+    let fName = await inquirer.prompt({
+        type: 'input',
+        name: 'fullName',
+        message: 'What is Your Name ?',
+    });
+    for (let i = 0; i < data.results.length; i++) {
+        let answer = [
+            ...data.results[i].incorrect_answers,
+            data.results[i].correct_answer,
+        ];
+        let ans = await inquirer.prompt({
+            type: 'list',
+            name: 'quiz',
+            message: data.results[i].question,
+            choices: answer.map((val) => val),
+        });
+        if (ans.quiz === data.results[i].correct_answer) {
+            ++score;
+        }
+    }
+    console.log(`${fName.fullName} your score is ${score}`);
 };
-export {};
+startQuiz();
